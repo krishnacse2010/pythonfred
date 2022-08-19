9078bdfc8cd366be82c576e56d59b7a98883eaa5
KrishnaKumar Mahadevan <kmahadev@microsoft.com>
2022-08-19 11:41:36 +1000

    integer to base encoding

---
+++ integerBase.py
@@ -0,0 +1,26 @@
+def from_base10(n, b):
+    if b < 2:
+        raise ValueError('Base b must be >= 2')
+    if n < 0:
+        raise ValueError('Number n must be >= 0')
+    if n == 0:
+        return [0]
+    digits = []
+    while n > 0:
+        m = n % b
+        n = n // b
+   
+        digits.insert(0, m)
+    return digits
+
+def encode(digits, digit_map):
+    # we require that digit_map has at least as many
+    # characters as the max number in digits
+    if max(digits) >= len(digit_map):
+        raise ValueError("digit_map is not long enough to encode digits")
+    
+    # we'll see this later, but the following would be better:
+    encoding = ''.join([digit_map[d] for d in digits])
+    return encoding
+listEncode = from_base10(21,16)
+print(encode(listEncode,"01234567891bcdef"))
